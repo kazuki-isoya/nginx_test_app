32e39a444d4493fe0999d349c03fc1e179135606
moru more <moremoru.world21@gmail.com>
2020-03-12 13:34:07 +0900

    ユニコーンの設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,9 @@
-rails_root = File.expand_path('../../', __FILE__)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+worker_processes 4
+listen 'unix:///tmp/nginx.socket', backlog: 1024
 
-worker_processes 2
-working_directory rails_root
-
-listen "#{rails_root}/tmp/unicorn.sock"
-pid "#{rails_root}/tmp/unicorn.pid"
-
-stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.log"
-
-
-
-# /Users/morumore/workspace/nginx_test_app
+before_fork do |server,worker|
+    FileUtils.touch('/tmp/app-initialized')
+end
